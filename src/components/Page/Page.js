f74e6869ccaae5c4910bd914e9797fd002466041
jamieeedj <james.dejesus@qualitytrade.com>
2024-01-19 14:42:15 +0800

    page enhancements

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -2,19 +2,32 @@ import React from 'react';
 
 import {Box, Container} from '@chakra-ui/react';
 import PageHeader from './PageHeader';
+import PageFooter from './PageFooter';
 
 function Page(props) {
 	const {children} = props;
 
 	return (
 		<Container
-			maxW={['sm', '2xl', '3xl', '7xl']}
 			display='flex'
+			height='100vh'
 			flexDir='column'
+			maxW={['2xl', '3xl', '7xl']}
 		>
 			<PageHeader />
 
-			<Box as='main'>{children}</Box>
+			<Box
+				as='main'
+				width='100%'
+				flexGrow={1}
+				display='flex'
+				flexDir='column'
+				overflow='hidden'
+			>
+				{children}
+			</Box>
+
+			<PageFooter />
 		</Container>
 	);
 }
