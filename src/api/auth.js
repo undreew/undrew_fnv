8905226c150bfa4253932ae7f7cfde7e535309f1
jamieeedj <james.dejesus@qualitytrade.com>
2024-01-26 13:09:27 +0800

    added login api

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,12 +4,8 @@ export const postRegister = (formData) => {
 	return fetcher('POST', 'auth/register', formData);
 };
 
-export const postLoginEmail = (formData) => {
-	return fetcher('POST', 'auth/login-email', formData);
-};
-
-export const postLoginPassword = (formData) => {
-	return fetcher('POST', 'auth/login-password', formData);
+export const postAuthLogin = (formData) => {
+	return fetcher('POST', 'auth/login', formData);
 };
 
 export const postAuthLogout = () => {
