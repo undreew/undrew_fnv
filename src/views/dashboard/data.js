c08147893935560521004383c8654886ab7b45a1
jamieeedj <james.dejesus@qualitytrade.com>
2024-02-14 06:11:43 +0800

    added data items

--- src/views/dashboard/data.js
+++ src/views/dashboard/data.js
@@ -1,5 +1,9 @@
 import item1 from 'assets/items/image (8).png';
 import item2 from 'assets/items/image (10).png';
+import item3 from 'assets/items/image (11).png';
+import item4 from 'assets/items/image (12).png';
+import item5 from 'assets/items/image (14).png';
+import item6 from 'assets/items/image (13).png';
 
 const ITEMS = [
 	{
@@ -24,6 +28,54 @@ const ITEMS = [
 		price: '$95',
 		in_wishlist: false,
 		image: item2,
+		is_new: false,
+	},
+	{
+		name: 'Shirt Dress',
+		list_description: 'Turn it up Dress',
+		full_description:
+			'Versatile and universally flattering, our wrap blouse can be tied, draped, snapped and wrapped mulitple ways',
+		colors: ['black', 'blue', 'green'],
+		size: ['xl', 's', 'md', 'lg', 'xl'],
+		price: '$245',
+		in_wishlist: false,
+		image: item3,
+		is_new: false,
+	},
+	{
+		name: 'Rule Zip Jacket',
+		list_description: 'Turn it up Jacket',
+		full_description:
+			'Versatile and universally flattering, our wrap blouse can be tied, draped, snapped and wrapped mulitple ways',
+		colors: ['green', 'orange'],
+		size: ['xl', 's', 'md', 'lg', 'xl'],
+		price: '$199',
+		in_wishlist: false,
+		image: item4,
+		is_new: false,
+	},
+	{
+		name: 'New Age Linen',
+		list_description: 'Turn it up Pants',
+		full_description:
+			'Versatile and universally flattering, our wrap blouse can be tied, draped, snapped and wrapped mulitple ways',
+		colors: ['black', 'blue', 'green'],
+		size: ['xl', 's', 'md', 'lg', 'xl'],
+		price: '$180',
+		in_wishlist: true,
+		image: item5,
+		is_new: false,
+	},
+	{
+		name: 'Boss Pullover',
+		list_description: 'Turn it up Pullover',
+		full_description:
+			'Versatile and universally flattering, our wrap blouse can be tied, draped, snapped and wrapped mulitple ways',
+		colors: ['black', 'blue', 'green'],
+		size: ['xl', 's', 'md', 'lg', 'xl'],
+		price: '$280',
+		in_wishlist: true,
+		image: item6,
 		is_new: false,
 	},
 ];
