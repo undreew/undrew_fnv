9227ea81f3ae7e96fff56ea0d4864a47f2c7b0cc
james <jamesdejesus74@gmail.com>
2024-04-06 12:49:09 +0800

    init footer

--- src/components/Page/PageFooter.js
+++ src/components/Page/PageFooter.js
@@ -1,7 +1,43 @@
+import {
+	Box,
+	IconButton,
+	Input,
+	InputGroup,
+	InputRightElement,
+	SimpleGrid,
+	Text,
+} from '@chakra-ui/react';
 import React from 'react';
+import {FaArrowRight} from 'react-icons/fa';
 
 function PageFooter() {
-	return <div>PageFooter</div>;
+	return (
+		<SimpleGrid
+			columns={2}
+			p={5}
+			gap={5}
+			color='white'
+			bgColor='neutral.gray404040'
+		>
+			<Box>
+				<Text>Join Our Club, Get 15% Off For Your Birthday</Text>
+
+				<InputGroup>
+					<Input
+						variant='modimaFilled'
+						placeholder='Enter your email address'
+					/>
+
+					<InputRightElement>
+						<IconButton variant='text'>
+							<FaArrowRight />
+						</IconButton>
+					</InputRightElement>
+				</InputGroup>
+			</Box>
+			<Box>2</Box>
+		</SimpleGrid>
+	);
 }
 
 export default PageFooter;
--- src/views/landing/index.js
+++ src/views/landing/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {GuestLayout} from 'components/Layouts';
 
 function Index() {
-	return <GuestLayout header={true}></GuestLayout>;
+	return <GuestLayout header={true} footer={true}></GuestLayout>;
 }
 
 export default Index;
