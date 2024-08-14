3f2b1b177b45ce801f9db27242ccd03b27c308a0
jamieeedj <james.dejesus@qualitytrade.com>
2024-08-14 10:51:12 +0800

    implemented cart item viewing

--- src/components/Popover/CartPopover.js
+++ src/components/Popover/CartPopover.js
@@ -3,15 +3,19 @@ import {
 	PopoverBody,
 	PopoverContent,
 	PopoverTrigger,
-	PopoverCloseButton,
 } from '@chakra-ui/react';
+import {CloseIcon} from '@chakra-ui/icons';
+import {Box, HStack, Image} from '@chakra-ui/react';
+
 import React, {useEffect} from 'react';
 import {PulseLoader} from 'react-spinners';
 import {FaShoppingBag} from 'react-icons/fa';
 import {Center, IconButton, Text} from '@chakra-ui/react';
 
+import urlJoin from 'url-join';
 import PropTypes from 'prop-types';
-import {isEmpty} from 'lodash';
+import {IMAGE_URL} from 'constants/configs';
+import {first, isEmpty, startCase} from 'lodash';
 
 import {useCart} from 'contexts/CartContext';
 
@@ -24,6 +28,43 @@ CartPopover.propTypes = {
 	header: PropTypes.string,
 	body: PropTypes.string,
 };
+
+function CartPopoverItem({item = {}}) {
+	const {
+		size,
+		color,
+		product_id: product,
+		item_grand_total: total,
+		item_total_quantity: quantity,
+	} = item;
+	const {images, name} = product;
+
+	const {public_id} = first(images) || {};
+	const imageSrc = urlJoin(IMAGE_URL, public_id || '');
+
+	return (
+		<HStack>
+			<Image src={imageSrc} w='30%' h='30%' alt='Image' />
+
+			<Box flexGrow={1}>
+				<HStack justifyContent='space-between'>
+					<Text textStyle='h6'>{name}</Text>
+					<IconButton variant='ghost' size='xs'>
+						<CloseIcon />
+					</IconButton>
+				</HStack>
+
+				<Text>Size: {startCase(size)}</Text>
+				<Text>Color: {startCase(color)}</Text>
+
+				<HStack justifyContent='space-between'>
+					<Text>Quantity: {quantity}</Text>
+					<Text>{total}</Text>
+				</HStack>
+			</Box>
+		</HStack>
+	);
+}
 
 function CartPopover({header, body}) {
 	const {isFetching, data, onGetCart} = useCart();
@@ -41,24 +82,29 @@ function CartPopover({header, body}) {
 				</IconButton>
 			</PopoverTrigger>
 
-			<PopoverContent color='primary.main'>
-				<PopoverCloseButton />
-
+			<PopoverContent color='primary.main' height='fit-content'>
 				<PopoverBody>
-					<Center height='360px' display='flex' flexDirection='column'>
-						{isFetching ? (
+					{isFetching ? (
+						<Center display='flex' flexDirection='column'>
 							<PulseLoader loading color='#36d7b7' />
-						) : isEmpty(data) ? (
-							<>
-								<Text textStyle='h6'>{header}</Text>
-								<Text textStyle='bodySm'>{body}</Text>
-							</>
-						) : (
-							<>
-								<Text textStyle='bodySm'>Items</Text>
-							</>
-						)}
-					</Center>
+						</Center>
+					) : isEmpty(data) ? (
+						<Center display='flex' flexDirection='column'>
+							<Text textStyle='h6'>{header}</Text>
+							<Text textStyle='bodySm'>{body}</Text>
+						</Center>
+					) : (
+						<>
+							<Text textStyle='h5' textAlign='center'>
+								Your Cart
+							</Text>
+							<Box mt={5}>
+								{data.map((item, index) => {
+									return <CartPopoverItem key={index} item={item} />;
+								})}
+							</Box>
+						</>
+					)}
 				</PopoverBody>
 			</PopoverContent>
 		</Popover>
