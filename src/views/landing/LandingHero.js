9600e7b67bc7f844b2e34c8125d29da7534b96a9
un.drew <82213003+undreew@users.noreply.github.com>
2025-03-24 09:16:17 +0800

    landing-enhancements hero section

--- src/views/landing/LandingHero.js
+++ src/views/landing/LandingHero.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import {Box} from '@chakra-ui/react';
+import {Box, Button, Text} from '@chakra-ui/react';
 
 import styles from './LandingHero.module.scss';
 
 function LandingHero() {
-	return <Box className={styles['landing-hero']} />;
+	return (
+		<Box position='relative'>
+			<Box className={styles['landing-hero']} />
+			<Box
+				sx={{
+					top: '70%',
+					left: '10%',
+					position: 'absolute',
+				}}
+			>
+				<Text textStyle='h3' style={{fontStyle: 'italic'}} color='white'>
+					Elegance in simplicity
+					<br />
+					Earth's harmony
+				</Text>
+
+				<Button
+					sx={{
+						mt: 2,
+						borderRadius: 0,
+						fontsize: '14px',
+						fontFamily: 'heading',
+					}}
+					variant='solid'
+					size={['sm', 'md']}
+				>
+					New In
+				</Button>
+			</Box>
+		</Box>
+	);
 }
 
 export default LandingHero;
