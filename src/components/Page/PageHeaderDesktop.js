da32ad45af2041887eb2ac4bdb42f0f5961427b9
un.drew <82213003+undreew@users.noreply.github.com>
2025-03-28 09:54:29 +0800

    landing-enhancements2 finalized header desktop and added desktop user

--- src/components/Page/PageHeaderDesktop.js
+++ src/components/Page/PageHeaderDesktop.js
@@ -1,13 +1,15 @@
 import {Link} from 'react-router-dom';
-import {HStack} from '@chakra-ui/react';
 import React, {createElement} from 'react';
-import {FaRegUser, FaDashcube, FaRegHeart} from 'react-icons/fa';
+import {FaRegUser, FaRegHeart} from 'react-icons/fa';
+
+import {Divider, HStack} from '@chakra-ui/react';
 
 import {LinkLogo} from 'components/Links';
 import {ButtonIcon} from 'components/Buttons';
 import {CartPopover} from 'components/Popover';
 
 import PageHeaderSearch from './PageHeaderSearch';
+import PageHeaderDesktopUser from './PageHeaderDesktopUser';
 import PageHeaderDesktopLinks from './PageHeaderDesktopLinks';
 
 import {map} from 'lodash';
@@ -22,13 +24,6 @@ function PageHeaderDesktop(props) {
 			to: '/login',
 			label: 'Login',
 			icon: <FaRegUser />,
-			component: ButtonIcon,
-		},
-		isAuth && {
-			as: Link,
-			to: '/products',
-			label: 'Products',
-			icon: <FaDashcube />,
 			component: ButtonIcon,
 		},
 		{
@@ -52,6 +47,18 @@ function PageHeaderDesktop(props) {
 					const {component, ...rest} = item;
 					return createElement(component, {...rest, key: index});
 				})}
+
+				{isAuth && (
+					<Divider
+						orientation='vertical'
+						colorScheme='green'
+						size='lg'
+						h='20px'
+						w='10px'
+					/>
+				)}
+
+				{isAuth && <PageHeaderDesktopUser />}
 			</HStack>
 		</>
 	);
